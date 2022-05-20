dbe338a54141d66e85ed0f7f2c93048451cd7f68
Andres Silva <asilva.playtechla@gmail.com>
2022-05-20 12:31:38 -0500

    Se agrega Input y Output a los sub proyectos

--- projects/example-one/src/app/app.component.ts
+++ projects/example-one/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'example-one';
+  @Input() title: string;
+  @Output() eventClick: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor() {}
+
+  clickTitle(): void {
+    this.eventClick.emit(this.title);
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-micro-frontend';
+
+  title1 = 'project-one';
+  title2 = 'project-two';
+  title3 = 'project-three';
+
+  countExampleOne = 0;
+  countExampleTwo = 0;
+  couuntExampleThree = 0;
+
+  clickElement(event: CustomEvent): void {
+    console.log(event);
+
+    // switch (event.detail) type string example-one o example-two o example-three
+    switch (event.detail) {
+      case this.title1:
+        this.countExampleOne += 1;
+        break;
+      case this.title2:
+        this.countExampleTwo += 1;
+        break;
+      case this.title3:
+        this.couuntExampleThree += 1;
+        break;
+      default:
+        break;
+    }
+  }
 }
